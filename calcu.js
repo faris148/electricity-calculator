3779aff6dbaf13f018b39bed0fd3b33d6c2a7177
faris148 <118299753+faris148@users.noreply.github.com>
2024-09-11 16:59:14 +0300

    تم اصلاح مشكلة العرض على متصفح الجوال

--- calcu.js
+++ calcu.js
@@ -106,8 +106,8 @@ function init() {
 
     // إعداد المشهد والكاميرا
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.position.z = 150; // تعديل للعمق
+    camera = new THREE.PerspectiveCamera(100, width / height, 0.9, 1000);
+    camera.position.z = 100; // تعديل للعمق
 
     // إعداد المحرك
     renderer = new THREE.WebGLRenderer({ alpha: true }); // تفعيل الشفافية
@@ -116,10 +116,10 @@ function init() {
 
     // الرموز الحسابية
     const symbols = ['+', '-', '=', '×', '÷'];
-    const particleCount = 150;
+    const particleCount = 200;
     const particles = new THREE.BufferGeometry();
     const particlePositions = new Float32Array(particleCount * 3);
-    const colors = new Float32Array(particleCount * 3);
+    const colors = new Float32Array(particleCount * 9);
 
     for (let i = 0; i < particleCount; i++) {
         const x = (Math.random() - 0.5) * 800;
@@ -155,7 +155,7 @@ function animate() {
     requestAnimationFrame(animate);
 
     // حركة بطيئة للجسيمات
-    particleSystem.rotation.y += 0.001;
+    particleSystem.rotation.y += 0.0014;
 
     renderer.render(scene, camera);
 }
